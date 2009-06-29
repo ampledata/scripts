cf3ad4e3ae3e87a06c3262d91f564f07001580c1
galbrecht <gba@gregalbrecht.com>
2009-06-29 14:04:11 -0700

    /Users/galbrecht/bin/gitout.sh automatic commit

--- gitout.sh
+++ gitout.sh
@@ -2,6 +2,8 @@
 # script to automatically commit all new files and changes
 # (c)2009 greg albrecht (gba@gregalbrecht.com)
 
+cd ~/src/scripts
+
 git add *
 git commit -am "$0 automatic commit"
 git push
